f8cc86dc70b3a72ebabf66f01b6a52d98a3ef8d5
hewam <hamiltonhew@gmail.com>
2021-03-09 17:31:10 -0500

    some better cards

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -8,24 +8,26 @@ export class ProjectsService {
   constructor(){}
 
   projects = {
-    giggot: {
-      id: "giggot",
-      name: "GigGot",
-      logo: "",
-      subtitle: "The all in one live performance platform",
-      description: "GigGot is an app I made to help people find performing artists. It started as a simpler way to book my DJ friends for houseparties in college. It turned into a full booking management platform for bars, venues, and everyone else.",
-      cover: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/giggot_phones.png?alt=media&token=2a3881af-6c3e-4e16-9752-1e3ebe00fe95",
-      image: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/giggot_artist.png?alt=media&token=2e95d431-cd96-4c6c-8367-46b7d5b25766",
-      html: ``
-    },
     anonacy: {
       id: "anonacy",
       name: "Anonacy",
-      logo: "",
       subtitle: "Anonymity + Privacy",
       description: "Anonacy is a platform built to help people anonymize their information when they use services on the internet. It's core features include email masks, sms aliases, and identity generators.",
-      cover: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/RoundedIcon.png?alt=media&token=4ad122f2-d2e4-4cda-8a3e-e30474067cb5",
+      logo: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/anonacy-logo.png?alt=media&token=dedb9e9a-a9f6-4c23-9530-fd59bc0bba45",
+      icon: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/anonacy-icon.png?alt=media&token=f93e95c2-36ca-4e23-ace8-b0721eb5ef3a",
+      cover: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/anonacy-splash.png?alt=media&token=2df73a28-e4cb-45e3-b129-78938fe881cf",
       image: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/logo_icon.png?alt=media&token=07901b5e-cb34-4dfb-ba1e-3fdc64821297",
+      html: ``
+    },
+    giggot: {
+      id: "giggot",
+      name: "GigGot",
+      subtitle: "The all in one live performance platform",
+      description: "GigGot is an app I made to help people find performing artists. It started as a simpler way to book my DJ friends for houseparties in college. It turned into a full booking management platform for bars, venues, and everyone else.",
+      logo: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/giggot-logo-black.png?alt=media&token=84e146df-9f59-412d-84b4-4a01e757fd48",
+      icon: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/giggot-icon.png?alt=media&token=e267a5da-037e-40e2-a19d-425e4d440391",
+      cover: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/giggot-splash.png?alt=media&token=3b6d4e93-87ee-4cdd-b592-afa06d2ea0ef",
+      image: "https://firebasestorage.googleapis.com/v0/b/hewham-ionic.appspot.com/o/giggot_artist.png?alt=media&token=2e95d431-cd96-4c6c-8367-46b7d5b25766",
       html: ``
     },
   }
