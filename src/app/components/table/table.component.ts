0a85ae6d2a7f41d556e4b24275903f0c59a52064
hewham <hamiltonhew@gmail.com>
2022-11-19 17:32:44 +0000

    editorjs working (basic, no plugins)

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
+import EditorJS from '@editorjs/editorjs';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit{
+export class TableComponent implements OnInit, AfterViewInit {
 
-  // @Input('project') project: any;
+  @ViewChild('editor', {read: ElementRef, static: true}) editorElement: ElementRef;
+
+  private editor: EditorJS;
+
   @Input('items') items: any;
   @Output() onClick: EventEmitter<any> = new EventEmitter();
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.initEditor();
+  }
+
+  private initEditor(): void {
+    console.log("this.editorElement: ", this.editorElement);
+
+    this.editor = new EditorJS({
+      minHeight: 200,
+      holder: this.editorElement.nativeElement
+    })
+  }
   
 }
